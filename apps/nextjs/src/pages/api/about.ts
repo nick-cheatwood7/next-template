d480d68d21bf528e71f41439b786e6ef47a7d051
Nick Cheatwood <nick.cheatwood@gmail.com>
2022-11-05 12:03:23 -0500

    ci: cleaned code

--- apps/nextjs/src/pages/api/about.ts
+++ apps/nextjs/src/pages/api/about.ts
@@ -2,10 +2,10 @@ import crypto from "crypto";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-    return res.status(200).json({
-        time: new Date(),
-        id: crypto.randomUUID()
-    });
+  return res.status(200).json({
+    time: new Date(),
+    id: crypto.randomUUID(),
+  });
 };
 
 export default handler;
--- apps/nextjs/src/utils/http-common.ts
+++ apps/nextjs/src/utils/http-common.ts
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: process.env.VERCEL_URL || "http://localhost:3000/api",
-    headers: {
-        common: {
-            "Content-Type": "application/json"
-        }
-    }
+  baseURL: process.env.VERCEL_URL || "http://localhost:3000/api",
+  headers: {
+    common: {
+      "Content-Type": "application/json",
+    },
+  },
 });
